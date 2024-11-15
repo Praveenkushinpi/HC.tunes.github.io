c10caff2d661c55bb17f4a3b8a1a0f424d617fd4
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2024-11-15 06:37:52 +0530

    up

--- script.js
+++ script.js
@@ -80,4 +80,31 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
 document.getElementById('next').addEventListener('click', ()=>{
     if(songIndex>=9){
          songIndex = 0
+    }
+         else{
+                songIndex += 1;
+                    }
+                        audioElement.src = `songs/${songIndex+1}.mp3`;
+                            masterSongName.innerText = songs[songIndex].songName;
+                                audioElement.currentTime = 0;
+                                    audioElement.play();
+                                        masterPlay.classList.remove('fa-play-circle');
+                                            masterPlay.classList.add('fa-pause-circle');
 
+                                            })
+
+                                            document.getElementById('previous').addEventListener('click', ()=>{
+                                                if(songIndex<=0){
+                                                        songIndex = 0
+                                                            }
+                                                                else{
+                                                                        songIndex -= 1;
+                                                                            }
+                                                                                audioElement.src = `songs/${songIndex+1}.mp3`;
+                                                                                    masterSongName.innerText = songs[songIndex].songName;
+                                                                                        audioElement.currentTime = 0;
+                                                                                            audioElement.play();
+                                                                                                masterPlay.classList.remove('fa-play-circle');
+                                                                                                    masterPlay.classList.add('fa-pause-circle');
+                                                                                                    })
+    }
